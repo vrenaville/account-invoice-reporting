a76c7dadd9dcf4d19034be5113bf64f675c4ef6b
vrenaville <vincent.renaville@camptocamp.com>
2015-07-30 14:39:26 +0200

    [FIX] pep 8 + module renamed

--- account_invoice_webkit/__init__.py
+++ account_invoice_webkit/__init__.py
@@ -20,4 +20,3 @@
 ##############################################################################
 from . import invoice
 from . import report
-from . import product
--- account_invoice_webkit/report/account_invoice.py
+++ account_invoice_webkit/report/account_invoice.py
@@ -46,5 +46,5 @@ class AccountInvoice_Report(report_sxw.rml_parse):
 
 report_sxw.report_sxw('report.account.invoice.webkit',
                       'account.invoice',
-                      'invoice_webkit/report/account_invoice.mako',
+                      'account_invoice_webkit/report/account_invoice.mako',
                       parser=AccountInvoice_Report)
